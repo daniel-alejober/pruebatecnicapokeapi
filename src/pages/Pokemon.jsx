6e94d6515a922de5eef9f265eb6d24670e470b0b
Daniel Alejo <daniel-alejo94@hotmail.com>
2025-01-04 12:30:54 -0600

    Ajustando view pokemon

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -11,7 +11,12 @@ const Pokemon = () => {
   const navigate = useNavigate();
 
   return (
-    <>
+    <Box
+      sx={{
+        maxWidth: { xs: "95%", sm: "100%" },
+        height: { xs: "auto", sm: "100%" },
+      }}
+    >
       <span
         onClick={() =>
           navigate(data.prevurl.startsWith("/pokemon/") ? "/" : data.prevurl)
@@ -28,68 +33,83 @@ const Pokemon = () => {
       </span>
       <Stack
         direction="column"
-        sx={[
-          {
-            justifyContent: "center",
-            height: "calc((1 - var(--template-frame-height, 0)) * 100%)",
-            marginTop: "max(40px - var(--template-frame-height, 0px), 0px)",
-            minHeight: "100%",
-            width: {
-              xs: "90% !important",
-              sm: "90% !important",
-              md: "90% !important",
-            },
-            margin: "0 auto",
-          },
-          (theme) => ({
-            "&::before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              zIndex: -1,
-              inset: 0,
-              backgroundImage:
-                "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
-              backgroundRepeat: "no-repeat",
-              ...theme.applyStyles("dark", {
-                backgroundImage:
-                  "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
-              }),
-            },
-          }),
-        ]}
+        sx={{
+          justifyContent: "center",
+          minHeight: "100vh",
+          marginTop: "max(40px, 10vh)",
+          padding: 2,
+          maxWidth: "95%",
+          margin: "0 auto",
+          overflowY: "auto",
+          position: "relative",
+        }}
       >
         <Stack
           direction={{ xs: "column-reverse", md: "row" }}
           sx={{
             justifyContent: "center",
             p: 2,
-            mx: "auto",
+            marginX: "auto",
+            gap: 3,
           }}
         >
           <Stack
-            direction={{ xs: "column-reverse", md: "row" }}
             sx={{
-              justifyContent: "center",
-              p: { xs: 2, sm: 4 },
-              m: "auto",
-              gap: 3,
+              flexDirection: "column",
+              alignSelf: "center",
+              gap: 4,
+              minWidth: 450,
+              backgroundColor: "whitesmoke",
+              borderRadius: "10px",
+              padding: 3,
+              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+              height: "100%",
+              minHeight: "100%",
+              width: { xs: "90%" },
             }}
           >
-            <Stack
+            <Typography
+              variant="h5"
+              component="div"
+              textAlign="center"
               sx={{
-                flexDirection: "column",
-                alignSelf: "center",
-                gap: 4,
-                minWidth: 450,
-                backgroundColor: "whitesmoke",
-                borderRadius: "10px",
-                padding: 3,
-                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-                height: "100%",
-                minHeight: "100%",
+                marginBottom: 2,
+                fontWeight: "bold",
+                color: "primary.main",
               }}
             >
+              Información
+            </Typography>
+
+            <Typography
+              variant="h6"
+              component="div"
+              textAlign="center"
+              sx={{
+                color: "text.primary",
+                backgroundColor: "primary.light",
+                padding: 1,
+                borderRadius: "5px",
+              }}
+            >
+              Altura: {data.height}
+            </Typography>
+
+            <Typography
+              variant="h6"
+              component="div"
+              textAlign="center"
+              sx={{
+                color: "text.primary",
+                backgroundColor: "secondary.light",
+                padding: 1,
+                borderRadius: "5px",
+              }}
+            >
+              Peso: {data.weight} kg
+            </Typography>
+
+            <div>
               <Typography
                 variant="h5"
                 component="div"
@@ -100,200 +120,157 @@ const Pokemon = () => {
                   color: "primary.main",
                 }}
               >
-                Información
+                Stats
               </Typography>
 
-              <Typography
-                variant="h6"
-                component="div"
-                textAlign="center"
-                sx={{
-                  color: "text.primary",
-                  backgroundColor: "primary.light",
-                  padding: 1,
-                  borderRadius: "5px",
-                }}
-              >
-                Altura: {data.height}
-              </Typography>
-
-              <Typography
-                variant="h6"
-                component="div"
-                textAlign="center"
-                sx={{
-                  color: "text.primary",
-                  backgroundColor: "secondary.light",
-                  padding: 1,
-                  borderRadius: "5px",
-                }}
-              >
-                Peso: {data.weight} kg
-              </Typography>
-
-              <div>
-                <Typography
-                  variant="h5"
-                  component="div"
-                  textAlign="center"
-                  sx={{
-                    marginBottom: 2,
-                    fontWeight: "bold",
-                    color: "primary.main",
-                  }}
+              {data.stats.map((item, index) => (
+                <Stack
+                  key={index}
+                  direction="row"
+                  alignItems="center"
+                  sx={{ gap: 2 }}
                 >
-                  Stats
-                </Typography>
-
-                {data.stats.map((item, index) => (
-                  <Stack
-                    key={index}
-                    direction="row"
-                    alignItems="center"
-                    sx={{ gap: 2 }}
-                  >
-                    <Typography
-                      sx={{
-                        fontWeight: "medium",
-                        minWidth: 100,
-                        textTransform: "capitalize",
-                      }}
-                    >
-                      {item.stat.name}:
-                    </Typography>
-                    <LinearProgress
-                      variant="determinate"
-                      value={item.base_stat}
-                      sx={{
-                        flex: 1,
-                        height: 10,
-                        borderRadius: "5px",
-                        backgroundColor: "grey.200",
-                      }}
-                    />
-                    <Typography
-                      variant="body2"
-                      sx={{
-                        color: "text.secondary",
-                        minWidth: 40,
-                        textAlign: "center",
-                      }}
-                    >
-                      {item.base_stat}
-                    </Typography>
-                  </Stack>
-                ))}
-              </div>
-
-              <div>
-                <Typography
-                  variant="h5"
-                  component="div"
-                  textAlign="center"
-                  sx={{
-                    marginBottom: 2,
-                    fontWeight: "bold",
-                    color: "primary.main",
-                  }}
-                >
-                  Habilidades
-                </Typography>
-                {data.abilities.map((item, index) => (
                   <Typography
-                    key={index}
-                    gutterBottom
                     sx={{
                       fontWeight: "medium",
-                      textAlign: "center",
+                      minWidth: 100,
                       textTransform: "capitalize",
-                      padding: 1,
-                      backgroundColor: "secondary.light",
-                      borderRadius: "5px",
-                      marginBottom: 1,
                     }}
                   >
-                    {item.ability.name}
+                    {item.stat.name}:
                   </Typography>
-                ))}
-              </div>
-            </Stack>
+                  <LinearProgress
+                    variant="determinate"
+                    value={item.base_stat}
+                    sx={{
+                      flex: 1,
+                      height: 10,
+                      borderRadius: "5px",
+                      backgroundColor: "grey.200",
+                    }}
+                  />
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      color: "text.secondary",
+                      minWidth: 40,
+                      textAlign: "center",
+                    }}
+                  >
+                    {item.base_stat}
+                  </Typography>
+                </Stack>
+              ))}
+            </div>
 
-            <Stack
-              sx={{
-                flexDirection: "column",
-                alignSelf: "center",
-                gap: 4,
-                minWidth: 450,
-                background:
-                  data.types.length > 0
-                    ? `linear-gradient(135deg, ${getColorByType(
-                        data.types[0].type.name
-                      )} 0%, #444 100%)`
-                    : "#555",
-                borderRadius: "20px",
-                padding: 4,
-                boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.3)",
-              }}
-            >
+            <div>
               <Typography
-                variant="h4"
+                variant="h5"
                 component="div"
                 textAlign="center"
                 sx={{
                   marginBottom: 2,
                   fontWeight: "bold",
-                  color: "#fff",
-                  textTransform: "uppercase",
-                  letterSpacing: "2px",
-                  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+                  color: "primary.main",
                 }}
               >
-                {capitalizeLetter(data.name)} #{data.order}
+                Habilidades
               </Typography>
+              {data.abilities.map((item, index) => (
+                <Typography
+                  key={index}
+                  gutterBottom
+                  sx={{
+                    fontWeight: "medium",
+                    textAlign: "center",
+                    textTransform: "capitalize",
+                    padding: 1,
+                    backgroundColor: "secondary.light",
+                    borderRadius: "5px",
+                    marginBottom: 1,
+                  }}
+                >
+                  {item.ability.name}
+                </Typography>
+              ))}
+            </div>
+          </Stack>
 
-              <Box
-                sx={{
-                  width: "100%",
-                  height: 250,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  overflow: "hidden",
-                  borderRadius: "15px",
+          <Stack
+            sx={{
+              flexDirection: "column",
+              alignSelf: "center",
+              gap: 4,
+              minWidth: 450,
+              background:
+                data.types.length > 0
+                  ? `linear-gradient(135deg, ${getColorByType(
+                      data.types[0].type.name
+                    )} 0%, #444 100%)`
+                  : "#555",
+              borderRadius: "20px",
+              padding: 4,
+              boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.3)",
+            }}
+          >
+            <Typography
+              variant="h4"
+              component="div"
+              textAlign="center"
+              sx={{
+                marginBottom: 2,
+                fontWeight: "bold",
+                color: "#fff",
+                textTransform: "uppercase",
+                letterSpacing: "2px",
+                textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+              }}
+            >
+              {capitalizeLetter(data.name)} #{data.order}
+            </Typography>
+
+            <Box
+              sx={{
+                width: "100%",
+                height: 250,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                overflow: "hidden",
+                borderRadius: "15px",
+              }}
+            >
+              <img
+                src={
+                  data.sprites.other?.dream_world?.front_default
+                    ? data.sprites.other?.dream_world?.front_default
+                    : data.sprites.front_default
+                }
+                alt={data.name}
+                style={{
+                  objectFit: "contain",
+                  maxWidth: "80%",
+                  maxHeight: "80%",
+                  filter: "drop-shadow(0px 8px 10px rgba(0, 0, 0, 0.3))",
+                  transition: "transform 0.3s ease, filter 0.3s ease",
+                  cursor: "pointer",
                 }}
-              >
-                <img
-                  src={
-                    data.sprites.other?.dream_world?.front_default
-                      ? data.sprites.other?.dream_world?.front_default
-                      : data.sprites.front_default
-                  }
-                  alt={data.name}
-                  style={{
-                    objectFit: "contain",
-                    maxWidth: "80%",
-                    maxHeight: "80%",
-                    filter: "drop-shadow(0px 8px 10px rgba(0, 0, 0, 0.3))",
-                    transition: "transform 0.3s ease, filter 0.3s ease",
-                    cursor: "pointer",
-                  }}
-                  onMouseEnter={(e) => {
-                    e.currentTarget.style.transform = "scale(1.1)";
-                    e.currentTarget.style.filter =
-                      "drop-shadow(0px 12px 20px rgba(0, 0, 0, 0.4))";
-                  }}
-                  onMouseLeave={(e) => {
-                    e.currentTarget.style.transform = "scale(1)";
-                    e.currentTarget.style.filter =
-                      "drop-shadow(0px 8px 10px rgba(0, 0, 0, 0.3))";
-                  }}
-                />
-              </Box>
-            </Stack>
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.transform = "scale(1.1)";
+                  e.currentTarget.style.filter =
+                    "drop-shadow(0px 12px 20px rgba(0, 0, 0, 0.4))";
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.transform = "scale(1)";
+                  e.currentTarget.style.filter =
+                    "drop-shadow(0px 8px 10px rgba(0, 0, 0, 0.3))";
+                }}
+              />
+            </Box>
           </Stack>
         </Stack>
       </Stack>
-    </>
+    </Box>
   );
 };
 
